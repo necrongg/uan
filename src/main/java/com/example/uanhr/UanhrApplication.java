9329d5b540dc73cd28ffb551c816041d0b75cb23
necrongg <necron7577@daum.net>
2025-07-19 19:27:55 +0900

    초기값

---
+++ src/main/java/com/example/uanhr/UanhrApplication.java
@@ -0,0 +1,13 @@
+package com.example.uanhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UanhrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UanhrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uanhr/UanhrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uanhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UanhrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
