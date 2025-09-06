aa50323764af803e1ded85ea0fb76169cbb750ec
necrongg <necron7577@daum.net>
2025-09-06 22:24:31 +0900

    중간세이브

--- src/main/uanhr/src/App.js
+++ src/main/uanhr/src/App.js
@@ -1,28 +1,32 @@
 import React, { useState } from "react";
 import NasUpload from "./components/NasUpload";
 import NasGallery from "./components/NasGallery";
-
-import { UploadOutlined } from '@ant-design/icons';
-import { Button, Upload } from 'antd';
-
-
+import { Button, message } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 
 function App() {
     const [isUploadOpen, setIsUploadOpen] = useState(false);
+    const [messageApi, contextHolder] = message.useMessage();
 
     return (
         <div>
+            {contextHolder} {/* 메시지 표시 영역 */}
             <div style={{ display: "flex", justifyContent: "space-between", padding: "10px 20px" }}>
                 <h2>갤러리</h2>
                 <div>
-                    <Upload disabled>
-                        <Button icon={<UploadOutlined />} onClick={() => setIsUploadOpen(true)}>사진 올리기</Button>
-                    </Upload>
+                    <Button icon={<UploadOutlined />} onClick={() => setIsUploadOpen(true)}>
+                        사진 올리기
+                    </Button>
                 </div>
             </div>
 
             {/* 업로드 모달 */}
-            {isUploadOpen && <NasUpload onClose={() => setIsUploadOpen(false)} />}
+            {isUploadOpen && (
+                <NasUpload
+                    onClose={() => setIsUploadOpen(false)}
+                    messageApi={messageApi} // 상위에서 메시지 전달
+                />
+            )}
 
             {/* 갤러리 */}
             <NasGallery />
